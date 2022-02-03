e41e3a3a58543868e75950dd11f9498a419d5d97
alexroumi <alexroumeli@gmail.com>
2022-02-03 22:37:18 +0200

    footer init

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
 <html>
 <?php
-$page_title = "Σχεδιασμός και Ανάπτυξη Διαδικτυακών Εφαρμογών - 2021-2022 - ΓΠΛ1"; include("header.php");
+$page_title = "Σχεδιασμός και Ανάπτυξη Διαδικτυακών Εφαρμογών - 2021-2022 - ΓΠΛ1";
+include("header.php");
 ?>
-  <body>
- <header>
-      <div class="header">
-        <p><?php echo $page_title; ?></p>
-      </div>
+
+<body>
+    <header>
+        <div class="header">
+            <p><?php echo $page_title; ?></p>
+        </div>
     </header>
     <div class="container">
-    <aside>
-    <ul>
-    <li>Φυλλάδιο 2:</li>
-    <ul>
-    <li><a href="f2ask1.php" target="_blank">Άσκηση 1</a></li>
-    <li><a href="f2ask2.php" target="_blank">Άσκηση 3</a></li>
-    </ul>
-    <li>Φυλλάδιο 3:</li>
-    <ul>
-    <li><a href="f3ask1.php" target="_blank">Άσκηση 1</a></li>
-    </ul>
-    </ul>
-    </aside>
-    <main>
-    Εδώ θα βρείτε όλες τις ασκήσεις από τα Φυλλάδια που έχουμε κάνει στο μάθημα "Σχεδιασμός και Ανάπτυξη Διαδικτυακών Εφαρμογών" όπου διδάσκεται η γλώσσα <strong>PHP</strong> με τους καθηγητές <strong>Βαμβακάρη Μιχαήλ</strong> και <strong>? Καρακίτσου</strong>
-    </main>
+        <aside>
+            <ul>
+                <li>Φυλλάδιο 2:</li>
+                <ul>
+                    <li><a href="f2ask1.php" target="_blank">Άσκηση 1</a></li>
+                    <li><a href="f2ask2.php" target="_blank">Άσκηση 3</a></li>
+                </ul>
+                <li>Φυλλάδιο 3:</li>
+                <ul>
+                    <li><a href="f3ask1.php" target="_blank">Άσκηση 1</a></li>
+                </ul>
+            </ul>
+        </aside>
+        <main>
+            Εδώ θα βρείτε όλες τις ασκήσεις από τα Φυλλάδια που έχουμε κάνει στο μάθημα "Σχεδιασμός και Ανάπτυξη
+            Διαδικτυακών Εφαρμογών" όπου διδάσκεται η γλώσσα <strong>PHP</strong> με τους καθηγητές <strong>Βαμβακάρη
+                Μιχαήλ</strong> και <strong>? Καρακίτσου</strong>
+        </main>
     </div>
     <footer>
+        Δημιουργήθηκε και συντηρείται από τον Αλέξανδρο Ρουμελιώτη.
     </footer>
-  </body>
+</body>
+
 </html>
