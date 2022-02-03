f89e860431bbc84c7da71ef2ff17b3ae9afd5932
Alexander Roumeliotis <59561790+alexroumi@users.noreply.github.com>
2022-02-03 20:10:36 +0000

    header update

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
-    <link
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <link rel="stylesheet" href="main.css">
-    
+
     <script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>
     <link href="code.css" rel="stylesheet" />
-  </head>
+</head>
