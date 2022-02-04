d1a1c9b78685c7721d8b772aaec58c8c28e1cebf
alexroumi <alexroumeli@gmail.com>
2022-02-04 17:14:06 +0200

    fix db

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $db = new SQLite3('db/users.db');
 $db->exec("CREATE TABLE IF NOT EXISTS live_users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ip TEXT,
+    ip VARCHAR(27) NOT NULL,
 );");
 $db->exec("INSERT INTO live_users (ip) VALUES ('" . $_SERVER['REMOTE_ADDR'] . "');");
 if ($_POST['type'] == 'unload') {
@@ -11,7 +11,7 @@ if ($_POST['type'] == 'unload') {
 
 $res = $db->query('SELECT * FROM live_users');
 while ($row = $res->fetchArray()) {
-    echo "{$row['id']} {$row['ip']} \n";
+    echo "{$row['id']} {$row['ip']} <br />";
 }
 
-$db -> close();
+$db->close();
